9f7b19271d5d5b80eb048a21d04da625f154fa44
Muhammad Alam <malam53345@gmail.com>
2023-10-24 15:04:33 +0500

    done

--- data/experienceData.js
+++ data/experienceData.js
@@ -3,7 +3,7 @@ export const experienceData = [
     id: 1,
     role: "Web Developer",
     company: "Da Wedding Whisper (StartUp)",
-    date: "(April 2023 - present )",
-    desc: "I am currently working as a Front-End Web Developer at Da Wedding Whisper, a startup, where I focus on creating visually appealing and user-friendly website.",
+    date: "(April 2023 - Sep 2023)",
+    desc: "I proudly served as a Front-End Web Developer at Da Wedding Whisper, a startup, where I focus on creating visually appealing and user-friendly website.",
   },
 ];
